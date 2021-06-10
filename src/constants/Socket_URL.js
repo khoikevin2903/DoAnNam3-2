a14330a74f6e76a608fea5665166db94acd3aa85
Student-ID-Card-Identification <khoikevin2903@gmail.com>
2021-06-10 16:16:22 +0700

    Updates

--- src/constants/Socket_URL.js
+++
@@ -1,1 +0,0 @@
-export const SOCKET_URL = 'https://chatchit69.herokuapp.com/websocket-chat';
--- src/constants/Transport.js
+++
@@ -1,3 +0,0 @@
-const transport = ["Ô tô", "Xe máy", "Xe Khách", "Máy Bay", "Tàu thủy"];
-
-export default transport;
